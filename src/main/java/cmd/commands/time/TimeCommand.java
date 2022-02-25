f72bf331ecb4d658209be2650037a9e06d1ff5fb
nurmersb <nils.urmersbach@capgemini.com>
2022-02-25 15:35:43 +0100

    Adding format option to time command

--- src/main/java/cmd/commands/time/TimeCommand.java
+++ src/main/java/cmd/commands/time/TimeCommand.java
@@ -6,18 +6,25 @@ import picocli.CommandLine;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@CommandLine.Command(
+import static picocli.CommandLine.*;
+
+@Command(
         name = "time",
         description = "command can be used to print current time",
         mixinStandardHelpOptions = true)
 public class TimeCommand implements Runnable {
+
+    @Option(names = {"-f", "--format"},
+            description =  "output current time in desired format",
+            defaultValue = "HH:mm")
+    String timeFormat;
 
     public TimeCommand() {
     }
 
     @Override
     public void run() {
-        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern(timeFormat)));
     }
 
 }
